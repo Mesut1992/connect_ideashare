6c8fed9ae1a6bade254d4c490a9a956dbbad1792
Mesut Kuscu <mesut.kuscu@hp.com>
2015-06-19 00:01:42 -0700

    addes file api full

--- includes/profile.inc.php
+++ includes/profile.inc.php
@@ -14,7 +14,7 @@ else {
 }
 
 $prep_stmt = "SELECT id, email, surname,
-    		lastname, faculty, linkin, location, webpage, facebook, twitter
+    		lastname, faculty, linkin, location, webpage, facebook, twitter, pic
     		FROM members WHERE id = ?;";
 
 $stmt = $mysqli->prepare($prep_stmt);
@@ -22,7 +22,7 @@ $stmt = $mysqli->prepare($prep_stmt);
 $stmt->bind_param('i', $uid);
 
 $stmt->execute();
-$stmt->bind_result($uid, $iemail, $ifname, $ilname, $ifac, $ilin, $iloc, $iwebp, $ifb, $itwit);
+$stmt->bind_result($uid, $iemail, $ifname, $ilname, $ifac, $ilin, $iloc, $iwebp, $ifb, $itwit, $ipic);
 
 $updated = false;
 
@@ -36,7 +36,8 @@ if($stmt->fetch()) {
 	$lin = $ilin;
 	$fb = $ifb;
 	$twit = $itwit;
-	
+	$pic = $ipic;
+
 	$stmt->close();
 	
 	if((isset($_GET["firstname"]) || isset($_GET["lastname"]) || isset($_GET["email"])
@@ -93,4 +94,80 @@ else {
 
 $sessuser = $_SESSION["username"];
 
+
+
+$error_msg = "";
+$error_msg_final = "";
+$uploadOk = 1;
+$updated = false;
+$error = false;
+
+if(isset($_GET["updated"])){
+    $updated = $_GET["updated"];
+}
+if(isset($_GET["error"])){
+    $error = $_GET["error"];
+}
+if(isset($_GET["errmsg"])){
+    $error_msg = $_GET["errmsg"];
+}
+
+if (isset($_POST['submit'])) {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);    
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+    
+    // Check if image file is a actual image or fake image
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        $uploadOk = 1;
+    } else {
+        $error_msg ="File is not an image.";
+        $uploadOk = 0;
+    }
+
+    // File already exists
+    if (file_exists($target_file)) {
+    $error_msg = "Sorry, file already exists.";
+    $uploadOk = 0;
+    }
+     // Check file size
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+    $error_msg ="Sorry, your file is too large.";
+    $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif" ) {
+        $error_msg ="Sorry, please upload a picture! Only JPG, JPEG, PNG and GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+         header('Location: start-share.php?error=true&errmsg='.$error_msg);
+    } 
+    else {
+            //target_file is the path to the pic
+           $target_file = $target_dir . "Pic_" . $_SESSION["uploadedpictures"] . "User_" . $_SESSION["user_id"] . '.' . $imageFileType ;
+         if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
+            {
+                $success_msg = "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+                $_SESSION["uploadedpictures"] = $_SESSION["uploadedpictures"] + 1;
+                //increase number of pics count of user in database
+                if ($insert_stmt = $mysqli->prepare("UPDATE members SET uploadedPics= ?, pic=? WHERE id =?;")) {
+                    $insert_stmt->bind_param('sss', $_SESSION["uploadedpictures"],$target_file, $_SESSION["user_id"]);
+                    // Execute the prepared query
+                    $insert_stmt->execute();
+                    header('Location: profile.php');
+           
+                }
+            }
+         else {
+            $error_msg_final = "Sorry, there was an error uploading your file.";
+            $uploadOk = false;
+        	}
+    	}
+    }
 ?>
